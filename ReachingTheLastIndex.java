dc64f673a250dc4817316a110a8425e90ea54cfd
Manasa Hari <harimanasa17@gmail.com>
2020-02-09 18:17:59 -0800

    Update and rename sample.java to ReachingTheLastIndex.java

---
+++ ReachingTheLastIndex.java
@@ -0,0 +1,18 @@
+// Time Complexity :O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :No
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public boolean canJump(int[] nums) {
+        int lastPosition=nums.length-1;
+        for(int i=nums.length-1; i>=0; i--){
+            if(i+nums[i]>=lastPosition){
+                lastPosition=i;
+            }
+        }
+        return lastPosition==0;
+    }
+}
